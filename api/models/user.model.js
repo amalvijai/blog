3c92fb0c574b2240fc99bfe09652b9268d5418cf
Amal Vijay <141136346+amalvijai@users.noreply.github.com>
2024-02-25 02:43:44 +0530

    creater user model

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import { constants } from 'buffer';
+import mongoose from 'mongoose';
+import mangoose from 'mongoose';
+
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required :true,
+        unique :true,
+
+    },
+    email:{
+        type: String,
+        required :true,
+        unique :true,
+
+    },
+    password:{
+        type: String,
+        required :true,
+       
+
+    },
+},{timestamps:true}
+);
+const User =mongoose.model('User',userSchema)
+export default User;
