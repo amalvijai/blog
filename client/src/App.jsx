f227df87eaa8f8a5b13c767b2fda265faa80ee43
Amal Vijay <141136346+amalvijai@users.noreply.github.com>
2024-02-25 01:26:00 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Singin from './pages/Singin'
 import Singup from './pages/Singup'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
  
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>} />
