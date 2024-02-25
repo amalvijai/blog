875ba4b20b75b8fb111a1d9403008ef498743f99
Amal Vijay <141136346+amalvijai@users.noreply.github.com>
2024-02-25 18:02:38 +0530

    add functionality to the sign up page

--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
-import { Label, TextInput,Button } from 'flowbite-react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Label, TextInput,Button, Alert, Spinner } from 'flowbite-react';
+import { Link,useNavigate } from 'react-router-dom'
+
 export default function Singup() {
+  const [formData, setFormData] =useState({});
+  const [errorMessage,setErrorMessage]= useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange =(e)=>{
+  setFormData({...formData,[e.target.id]: e.target.value.trim() });
+  };
+ const handleSumbit =async (e)=>{
+  e.preventDefault();
+  if(!formData.username || !formData.email || !formData.password){
+    return setErrorMessage('Please fill out all fields');
+  }
+  try{
+    setLoading(true);
+    setErrorMessage(null);
+     const res = await fetch('api/auth/singup' ,{
+      method:'POST',
+      headers: {'Content-Type': 'application/json'},
+      body:JSON.stringify(formData)
+     });
+     const data = await res.json();
+     if(data.sucess === false ){
+      return setErrorMessage(data.message);
+     }
+     setLoading(false);
+     if(res.ok){
+      navigate('/singin');
+     }
+  } catch (error){
+    setErrorMessage(error.message);
+    setLoading(false);
+  }
+ };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 '>
@@ -18,13 +52,13 @@ export default function Singup() {
       </div>
       {/* right */}
       <div className='flex-1'>
-        <form  className='flex flex-col gap-4'>
+        <form  className='flex flex-col gap-4' onSubmit={handleSumbit}>
           <div >
           <Label value='Your username'/>
           <TextInput 
           type='text'
           placeholder='Username'
-          id='username'/>
+          id='username'onChange={handleChange} />
       
           </div>
           <div >
@@ -32,19 +66,27 @@ export default function Singup() {
           <TextInput 
           type='text'
           placeholder='dev@comapny.com'
-          id='username'/>
+          id='email' onChange={handleChange} />
       
           </div>
           <div >
           <Label value='Your password'/>
           <TextInput 
-          type='text'
+          type='password'
           placeholder='Password'
-          id='password'/>
+          id='password' onChange={handleChange}/>
       
           </div>
-          <Button gradientDuoTone='purpleToPink' type='submit'>
-            Sign Up
+          <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+            {
+              loading ? (
+              <>  <Spinner  size='sm'/>
+              <span className='pl-3'>
+                Loading...
+              </span>
+              </>
+              ): 'Sing Up'
+            }
           </Button>
         </form>
         <div className='flex gap-2 text-sm mt-5'>
@@ -53,6 +95,13 @@ export default function Singup() {
             Sign In
           </Link>
         </div>
+        {
+          errorMessage && (
+            <Alert className='mt-5 'color='failure'>
+                  {errorMessage}
+            </Alert>
+          )
+        }
       </div>
       
       </div>
