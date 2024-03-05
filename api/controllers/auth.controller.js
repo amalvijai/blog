989ba554bbbf740d23eab49f84d578cec6590a9c
Amal Vijay <141136346+amalvijai@users.noreply.github.com>
2024-03-05 16:19:15 +0530

    OAuth functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
+
 import { errorHandler } from "../Utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
  import jwt from 'jsonwebtoken'
+ 
 
 export const singup =async (req,res,next)=>{
     const { username, email, password}=req.body;
@@ -54,5 +56,44 @@ export const singin = async ( req, res, next) => {
         .json(rest);
     } catch (error) {
          next(error);
+    }
+}
+export const google = async (req, res, next) => {
+
+    const {email, name, googlePhotoUrl}=req.body;
+    try {
+        const user = await User.findOne({email});
+        if(user){
+            const token =jwt.sign({id: user._id}, process.emv.JWT_SECRET);
+            const {password, ...rest} = user._doc;
+            res.status(200).cookie('access_token',token, {
+                httpOnly: true
+            }).json(rest);
+        }else{
+            const generatePassword = Math.random().toString(36).slice(-8 + Math.random().toString
+            (36).slice).slice(-8)
+            const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
+            const newUser = new User({
+                username: name.toLowerCase().split('').join('') + Math.random()
+                .toString(9).slice(-4),
+                email,
+                password: hashedPassword,
+               profilePicture: googlePhotoUrl,
+            });
+            await newUser.save();
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+            const {password, ...rest } =newUser._doc;
+            res
+            .status(200)
+            .cookie('access_token', token, {
+                httpOnly: true,
+            })
+            .json(rest);
+
+        }
+        
+    } catch (error) {
+        next(error)
+        
     }
 }
